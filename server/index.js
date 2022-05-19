090a0ccf5c90f3ee836e5dfa13fa98fcb9f6b39d
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-05-19 17:29:10 +0300

    Use cors in Server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 const WordsModel = require('./models/Words');
 
 
 app.use(express.json()); // receive information from fronted in json format
+app.use(cors()); // to communicate with APIs created in app, so cors policy wouldn't block requests
 
 mongoose.connect(
     "mongodb+srv://crud-tutorial:HYGrTLRiPlMVSdkP@crud.c24kg.mongodb.net/words?retryWrites=true&w=majority", 
