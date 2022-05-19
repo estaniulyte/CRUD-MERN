79c773b7cf1047286d6ab17c42772344d349757b
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-05-19 17:18:36 +0300

    Create form and states for posting data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [word, setWord] = useState('');
+  const [translation, setTranslation] = useState('');
+
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert", {
+      word: word,
+      translation: translation,
+    })
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD App with MERN</h1>
+
+      <label>Word to translate</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setWord(event.target.value);
+        }}
+      />
+      <label>Translation</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setTranslation(event.target.value);
+        }}
+      />
+      <button onClick={addToList}>Add To List</button>
     </div>
   );
 }
