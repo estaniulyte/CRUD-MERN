df157eaa223e9ab521d38237096a999df1891aa7
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-05-23 11:35:47 +0300

    Get data and Send to FrontEnd

--- server/index.js
+++ server/index.js
@@ -30,6 +30,16 @@ app.post('/insert', async (req, res) => {
     }
 })
 
+app.get('/read', async (req, res) => {
+    WordsModel.find({}, (err, result) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.send(result);
+    })
+})
+
 app.listen(3001, () => {
     console.log('Server running on port 3001...')
 });
